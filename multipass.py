66ca8ab272e15f866e3ca6fe95f5e9858c423c40
george <benlusamba@gmail.com>
2018-05-28 19:23:31 -0400

    File Write

--- multipass.py
+++ multipass.py
@@ -1,5 +1,6 @@
 #Multiple passwords:
 import random
+import prettytable
 
 inp = input('How many characters?: ')
 iterations = input('How many passwords: ')
@@ -10,3 +11,10 @@ for i in range (iter):
     s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
     p =  "".join(random.sample(s,len))
     print (p)
+
+
+#Write results as csv or txt
+result = str(p)
+output = open('passwords' + '.csv','w')
+output.write('%s' % result)
+output.close()
